adf048e44f929fd6a099a51169583981031e0063
jamie010323 <jamie010323@gmail.com>
2026-06-16 12:06:24 +0900

    ch03

---
+++ ch03/src/main/java/DTO/UserDTO.java
@@ -0,0 +1,20 @@
+package DTO;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+//@RequiredArgsConstructor
+@Builder
+public class UserDTO {
+
+    private String userid;
+    private String name;
+    private String birth;
+    private int age;
+
+
+}
